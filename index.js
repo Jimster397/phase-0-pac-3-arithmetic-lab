b408c11edb8bd5b2a081b5202750ed3c2590c377
James <jamesschmidt124@gmail.com>
2021-09-02 14:39:40 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+
+function add(a, b) {
+    let sum ;
+    sum = a + b;
+    return sum;
+}
+function subtract (a, b) {
+let sum ;
+sum = a - b;
+return sum;
+}
+function multiply (a, b){
+    let sum;
+    sum = a * b;
+return sum;
+
+}
+function divide (a, b) {
+let sum ;
+sum = a / b;
+return sum;
+}
+function increment (n) {
+    n++;
+    return n;
+}
+function decrement (n) {
+    n--;
+    return n;
+}
+function makeInt(n) {
+     
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+     return parseFloat(n);
+}
+
